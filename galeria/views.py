c6239b6adfeb42be1b17c57bb2976cc59017a174
Isabella Ramos <rmsisabella@gmail.com>
2024-11-10 11:46:44 -0300

    Passando referências e iniciando o Django Admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from galeria.models import Fotografia
 
 def index(request):
     
-    dados = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "webtelescope.org / NASA / James Web"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"}
-}
+    #dados = {
+    #1: {"nome": "Nebulosa de Carina",
+    #    "legenda": "webtelescope.org / NASA / James Web"},
+    #2: {"nome": "Galáxia NGC 1079",
+    #    "legenda": "nasa.org / NASA / Hubble"}
+#}
+    # return render(request, 'galeria/index.html', {"cards": dados})
     
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
