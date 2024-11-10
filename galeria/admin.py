4a551a58a3284af5414ad24f2e7c684440495c1c
Isabella Ramos <rmsisabella@gmail.com>
2024-11-10 12:03:17 -0300

    CRUD no Django Admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListaFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+# Registro do BD no admin
+admin.site.register(Fotografia, ListaFotografias)
