553b221019149ee7d2e019851d6600d7fd0bd9b1
Isabella Ramos <rmsisabella@gmail.com>
2024-12-14 14:08:52 -0300

    Finalização do módulo Django Admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListaFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada",)
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10
 
 # Registro do BD no admin
 admin.site.register(Fotografia, ListaFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 class Fotografia(models.Model):
@@ -13,7 +14,10 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
-    
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.datetime.now, blank=False)
+        
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        #é o que retorna ao salvar algo no django admin -> return f"Fotografia [nome={self.nome}]"
+        return self.nome
